4c3d9cbf72603f36390879c91480bfaafd6f50ca
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-04-17 00:54:42 +0100

    Added header component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/" component={ HomePage } />
           <Route path="/shop" component={ ShopPage } />

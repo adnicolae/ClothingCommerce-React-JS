9fa971794ecb8e1afbbd310d75e8423efc59188f
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-04-17 22:40:56 +0100

    Added sign-up page and login form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInUp from './pages/sign-in-up/sign-in-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={ HomePage } />
           <Route path="/shop" component={ ShopPage } />
+          <Route path="/signin" component={ SignInUp } />
         </Switch>
       </Router>
     </div>
---
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './sign-in-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInUpPage = () => {
+  return (
+    <div className="sing-in-and-sign-up">
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInUpPage;

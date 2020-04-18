9a661f22281213523c3d1a7cdfa20071a98389ce
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-04-19 00:23:08 +0100

    Finish sign in with google styling/header

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
     return (
       <div>
         <Router>
-          <Header />
+          <Header currentUser={ this.state.currentUser } />
           <Switch>
             <Route exact path="/" component={ HomePage } />
             <Route path="/shop" component={ ShopPage } />

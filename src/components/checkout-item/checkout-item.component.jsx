29c7187e7222005e82e1d8c4d602f511377e27e2
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-04-26 18:01:04 +0100

    Add addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
+
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, price, quantity } }) => {
+const CheckoutItem = ({ clearItem, cartItem, addItem, removeItem }) => {
+  const { imageUrl, name, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -9,12 +13,20 @@ const CheckoutItem = ({ cartItem: { imageUrl, name, price, quantity } }) => {
       </div>
       <span className="name">{ name }</span>
       <span className="quantity">
-        { quantity }
+        <div className="arrow" onClick={ () => removeItem(cartItem) }>&#10094;</div>
+        <span className="value">{ quantity }</span>
+        <div className="arrow" onClick={ () => addItem(cartItem) }>&#10095;</div>
       </span>
       <span className="price">{ price }</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={ () => clearItem(cartItem) }>&#10005;</div>
     </div>
   )
-}
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item  => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -28,7 +28,7 @@ const CheckoutPage = ({ cartItems, total }) => {
         </div>
       </div>
       {
-        cartItems.map(cartItem => <CheckoutItem key={ cartItems.id } cartItem={ cartItem } />)
+        cartItems.map(cartItem => <CheckoutItem key={ cartItem.id } cartItem={ cartItem } />)
       }
       <div className="total">
         <span>TOTAL: ${ total }</span>

5a6a11ba33ea4bdaa8d4117b3ea4cfa59927987f
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-04-26 18:58:05 +0100

    Refactor shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
   return (
     <div className="shop-page">
-      <div className="title">Collections</div>
-      {
-        collections.map(({ id, ...collectionProps }) => (
-          <CollectionPreview key={ id } { ...collectionProps } />
-        ))
-      }
+      <CollectionsOverview />
     </div>
   )
-}
+};
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;

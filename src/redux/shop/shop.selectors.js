cf679b6c757e1607ca2606306d463c7f89edd22b
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-04-27 14:46:48 +0100

    Updated flow for directory and shop, adding collection page for each collection

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -4,4 +4,9 @@ const selectshop = state => state.shop;
 
 export const selectShopCollections = createSelector([selectshop], shop => shop.collections); 
 
+export const selectCollectionsForPreview = createSelector([selectShopCollections], collections => Object.keys(collections).map(key => collections[key]));
+
 export const selectShopItems = createSelector([selectShopCollections], collection => collection.items);
+
+export const selectCollection = collectionUrlParam => 
+  createSelector([selectShopCollections], collections => collections[collectionUrlParam]);

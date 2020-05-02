342411cb0a406083469cd20861fa75b7b499a940
Andrei Nicolae <andrei.nicolae@vodafone.com>
2020-05-02 20:36:43 +0100

    Update shop data with collections data from firestore

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,7 +1,13 @@
 import SHOP_DATA from '../../cms/shop-data';
+import ShopActionTypes from './shop.types';
 
 export const shopReducer = (state = SHOP_DATA, action) => {
   switch(action.type) {
+    case ShopActionTypes.UPDATE_COLLECTIONS:
+      return {
+        ...state,
+        collections: action.payload
+      };
     default:
       return state;
   };
